2a8e48af114e37fa5529a2cf2d8810a2e7857a14
Daniel Morris <2089274m@student.gla.ac.uk>
2019-01-26 23:44:31 +0000

    chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from rango.models import Category, Page #imported for chapter 5 (database) work
 
-# Register your models here.
+admin.site.register(Category)
+##
+
+class PageAdmin (admin.ModelAdmin):
+    fields = ['category', 'url']
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
+
+#class QuestionAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None,               {'fields': ['question_text']}),
+#        ('Date information', {'fields': ['pub_date']}),
+#    ]
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
+
 MEDIA_URL = '/media/'
 
 print(__file__)
