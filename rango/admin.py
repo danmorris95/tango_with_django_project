759f400ca48428f4bbc06f7b3ccad12832eafa73
Daniel Morris <2089274m@student.gla.ac.uk>
2019-02-01 17:09:15 +0000

    chapter6

--- rango/admin.py
+++ rango/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from rango.models import Category, Page #imported for chapter 5 (database) work
 
-admin.site.register(Category)
+#admin.site.register(Category)
 ##
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 class PageAdmin (admin.ModelAdmin):
-    fields = ['category', 'url']
+    #fields = ['category', 'url']
     list_display = ('title', 'category', 'url')
 
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 #class QuestionAdmin(admin.ModelAdmin):
 #    fieldsets = [
--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+
+    try:
+
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+
+        context_dixt['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
 
 def index (request):
     
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.    
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    most_viewed_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'views' : most_viewed_list}
+
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     
-    return render(request, 'rango/index.html', context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict) #context =  necessary?
 
 
 
@@ -21,5 +51,5 @@ def index (request):
 
 def about (request):
 
-    context_dict2 = {'boldmessage': "SCRAMBLED EGG"}
-    return render(request, 'rango/about.html', context=context_dict2)
+ #   context_dict2 = {'boldmessage': "SCRAMBLED EGG"}
+    return render(request, 'rango/about.html')
